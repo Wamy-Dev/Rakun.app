deace524e750f2e8e14edde7a296c367957c5b76
David <25761442+Wamy-Dev@users.noreply.github.com>
2022-06-24 17:51:48 -0500

    simple frontend no functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,17 @@ import "@fontsource/montserrat"
 import { Center, TextInput, TextInputProps, ActionIcon, useMantineTheme, Container, SimpleGrid} from '@mantine/core';
 import { Search, ArrowRight, ArrowLeft } from 'tabler-icons-react';
 import rakun from "@lowlighter/rakun"
+import Raccoon from "./raccoon.svg"
 
 export function App() {
   return (
     <SimpleGrid cols={1} spacing="xl">
       <Center>
-        <img></img>
+        <h1>rakun.app</h1>
       </Center>
+      <Center>
+        <img src={Raccoon} style={{width: "200px"}}></img>
+        </Center>
       <Center>
         <TextInput
         placeholder="Input your anime torrent to view its metdata."
@@ -18,10 +22,10 @@ export function App() {
         color='#FCF3EA'
         style={{width: "32vw", minWidth: "320px"}}
         rightSection={
-          <ActionIcon size={32} radius="xl" color="gray[0]" variant="filled" onClick={() => {
+          <ActionIcon size={32} radius="xl" style={{backgroundColor: "grey"}} variant="filled" onClick={() => {
             console.log(rakun.parse("[Team246] Ghost in the shell Stand alone complex S01 E10-E15 [BDREMUX 1080P MULTi DTSHDMA 5.1][VOSTFR]"))
           }}>
-            {<ArrowRight size={18} />}
+            {<img src={Raccoon} style={{width: "25px"}}></img>}
           </ActionIcon>
         }
         rightSectionWidth={42}
